2d64114eff5fdfc1dcb38cbac04defa969a83fc2
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-06-03 20:39:14 +0300

    Feat(React) Added files to new project

--- src/components/Costs/CostsFilter.js
+++ src/components/Costs/CostsFilter.js
@@ -14,7 +14,6 @@ const CostsFilter = props => {
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
-          <option value="2019">2019</option>
         </select>
       </div>
     </div>
